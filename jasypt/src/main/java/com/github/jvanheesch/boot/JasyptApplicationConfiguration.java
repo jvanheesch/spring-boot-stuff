53b2e498cf4c6702f11db7787e6301535e8da1c8
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2019-01-02 19:36:39 +0100

    Created another-properties-file.properties containing encrypted properties.

--- jasypt/src/main/java/com/github/jvanheesch/boot/JasyptApplicationConfiguration.java
+++ jasypt/src/main/java/com/github/jvanheesch/boot/JasyptApplicationConfiguration.java
@@ -3,15 +3,18 @@ package com.github.jvanheesch.boot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.AbstractEnvironment;
 
 /**
  * See https://www.baeldung.com/spring-boot-jasypt
  */
 @Configuration
+//@EncryptablePropertySource and @PropertySource seem to behave the same.
+@PropertySource("classpath:another-properties-file.properties")
 public class JasyptApplicationConfiguration {
     @Bean
-    public Object testJasypt(AbstractEnvironment environment, @Value("${encryptedv3.property}") String decryptedValue) {
+    public Object testJasypt(AbstractEnvironment environment, @Value("${some.key.from.another.properties.file}") String decryptedValue) {
         System.out.println(environment.getProperty("encryptedv3.property"));
         System.out.println(decryptedValue);
         System.out.println(environment.getPropertySources());
