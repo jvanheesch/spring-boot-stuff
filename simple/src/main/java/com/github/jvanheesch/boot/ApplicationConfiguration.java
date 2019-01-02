6d241fa08f9559ab0dd37f457fa37224bbbf1043
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2019-01-02 17:49:19 +0100

    Added another-properties-file.properties.

--- simple/src/main/java/com/github/jvanheesch/boot/ApplicationConfiguration.java
+++ simple/src/main/java/com/github/jvanheesch/boot/ApplicationConfiguration.java
@@ -3,11 +3,16 @@ package com.github.jvanheesch.boot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:another-properties-file.properties")
 public class ApplicationConfiguration {
     @Value("${some.key.from.properties.file}")
     private String someString;
+
+    @Value("${some.key.from.another.properties.file}")
+    private String someStringFromAnotherPropertiesFile;
 
     @Bean
     public Object someBean(@Value("${some.key.from.properties.file}") String someBean) {
