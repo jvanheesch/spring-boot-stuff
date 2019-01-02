0f607404735f4e7c3203ccbd2a7e522cc2062fd2
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2019-01-02 19:41:56 +0100

    Cleanup jasypt - using spring autoconfiguration (jasypt-spring-boot-2.1.0.jar).

--- jasypt/src/main/java/com/github/jvanheesch/boot/JasyptApplicationConfiguration.java
+++ jasypt/src/main/java/com/github/jvanheesch/boot/JasyptApplicationConfiguration.java
@@ -1,12 +1,8 @@
 package com.github.jvanheesch.boot;
 
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.AbstractEnvironment;
 
 /**
@@ -14,23 +10,7 @@ import org.springframework.core.env.AbstractEnvironment;
  */
 @Configuration
 public class JasyptApplicationConfiguration {
-    @Bean(name = "encryptorBean")
-    public StringEncryptor stringEncryptor() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("password");
-        config.setAlgorithm("PBEWithMD5AndDES");
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setStringOutputType("base64");
-        encryptor.setConfig(config);
-        return encryptor;
-    }
-
     @Bean
-    @DependsOn("encryptorBean")
     public Object testJasypt(AbstractEnvironment environment, @Value("${encryptedv3.property}") String decryptedValue) {
         System.out.println(environment.getProperty("encryptedv3.property"));
         System.out.println(decryptedValue);
