d1ddfa8ac2a83aa652eb5d68173e4891d0335558
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2019-01-04 18:01:52 +0100

    Fixed.

--- jersey/src/main/java/com/github/jvanheesch/boot/jersey/JerseyConfig.java
+++ jersey/src/main/java/com/github/jvanheesch/boot/jersey/JerseyConfig.java
@@ -3,6 +3,11 @@ package com.github.jvanheesch.boot.jersey;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.ApplicationPath;
+
+// https://stackoverflow.com/a/29670751
+// both localhost:8080/jersey/hello/world and localhost:8080/actuator/health now work.
+@ApplicationPath("/jersey")
 @Component
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
