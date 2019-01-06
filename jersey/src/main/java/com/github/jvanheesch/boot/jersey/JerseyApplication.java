0dcc166f08646f81be443715163482ad8a2f11ce
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2019-01-06 19:12:16 +0100

    Added dependency on util + configured InfoFilter (see util).

--- jersey/src/main/java/com/github/jvanheesch/boot/jersey/JerseyApplication.java
+++ jersey/src/main/java/com/github/jvanheesch/boot/jersey/JerseyApplication.java
@@ -1,11 +1,25 @@
 package com.github.jvanheesch.boot.jersey;
 
+import com.github.jvanheesch.servlet.InfoFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JerseyApplication {
     public static void main(String[] args) {
         SpringApplication.run(JerseyApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<InfoFilter> infoFilter() {
+        FilterRegistrationBean<InfoFilter> filterRegistrationBean
+                = new FilterRegistrationBean<>();
+
+        filterRegistrationBean.setFilter(new InfoFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
     }
 }
