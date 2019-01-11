72ca1a8fae187c89faccc1fb76ae8a8e353cfae9
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2019-01-11 12:31:22 +0100

    Added invalidateSessionLink.

--- wicket-helloworld/src/main/java/com/github/jvanheesch/boot/HomePage.java
+++ wicket-helloworld/src/main/java/com/github/jvanheesch/boot/HomePage.java
@@ -1,7 +1,9 @@
 package com.github.jvanheesch.boot;
 
+import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 
 public class HomePage extends WebPage {
     private static final long serialVersionUID = -4628714388032191363L;
@@ -11,5 +13,14 @@ public class HomePage extends WebPage {
         super.onInitialize();
 
         this.add(new Label("message", new HomePageModel()));
+
+        this.add(new Link<Void>("invalidateSessionLink") {
+            private static final long serialVersionUID = -3109267727640748943L;
+
+            @Override
+            public void onClick() {
+                Session.get().invalidate();
+            }
+        });
     }
 }
