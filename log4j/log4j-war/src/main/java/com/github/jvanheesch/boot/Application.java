622049ed615bc8ae6139171a1767bd894e503ba1
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2019-01-22 13:30:40 +0100

    Deploying in container now uses spring boot's log4j2.xml.

--- log4j/log4j-war/src/main/java/com/github/jvanheesch/boot/Application.java
+++ log4j/log4j-war/src/main/java/com/github/jvanheesch/boot/Application.java
@@ -2,11 +2,12 @@ package com.github.jvanheesch.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(ApplicationConfiguration.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
